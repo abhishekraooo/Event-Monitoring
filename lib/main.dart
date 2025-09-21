b8f57cae7afd9061a6231201e38b702dd35950e8
Abhishek Rao K <studentabhishek25@gmail.com>
2025-09-21 19:04:19 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 // lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ideathon_monitor/screen/dashboard_screen.dart';
 import 'package:ideathon_monitor/screen/login_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  await Supabase.initialize(
-    url: dotenv.env['SUPABASE_URL']!,
-    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+
+  // Define the variables using String.fromEnvironment
+  // Vercel will provide these values during the build process.
+  const supabaseUrl = String.fromEnvironment('FLUTTER_PUBLIC_SUPABASE_URL');
+  const supabaseAnonKey = String.fromEnvironment(
+    'FLUTTER_PUBLIC_SUPABASE_ANON_KEY',
   );
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
   runApp(const MyApp());
 }
 
